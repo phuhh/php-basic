69a91b32486a4bb64c53f0ebee661853b16113b1
phuhh <huynhhieu.phu@gmail.com>
2024-06-17 23:19:16 +0700

    20260617 done loop while

--- Module02/_08_Loops/_01_for/ex01.php
+++ Module02/_08_Loops/_01_for/ex01.php
@@ -1,29 +1,29 @@
 <?php
 
 /**
- * Vòng lặp for (lập với số lần lặp được xác định trước)
+ * Vòng lặp for (lặp với số lần lặp được xác định trước)
  * 
  * for($ten-bien = gia-tri-bat-dau; dieu-kien-dung; bieu-thuc-tang-hoac-giam) {
  *    // code
  * }
  * 
- * Vòng lặp for tăng
+ * Ví dụ: Vòng lặp for tăng
  */
 
 $count = 10; // Xác định số lần lặp
 $start = 0; // Xác định giá trị bắt đầu
-
 for ($i = $start; $i < $count; $i++) {
     echo 'Đây là vòng lặp thứ ' . $i . '<br>';
 }
 echo '<hr>';
 
-// Tăng 2 đơn vị
+// Ví dụ: Tăng 2 đơn vị
 for ($i = $start; $i < $count; $i += 2) {
     echo 'Đây là vòng lặp thứ ' . $i . '<br>';
 }
 echo '<hr>';
-// VÍ DỤ: Tính tổng S = 1+2+3+4+5+...+n
+
+// Ví dụ: Tính tổng S = 1+2+3+4+5+...+n
 $number = 10;
 $result = 0; // Giả định giá trị ban đầu là 0
 for ($i = 1; $i < $number; $i++) {
@@ -32,10 +32,10 @@ for ($i = 1; $i < $number; $i++) {
 echo $result;
 
 /**
- * Giả Thích:
- * $result = 0;
+ * Giải thích:
+ * $result = 0; // Giá trị ban đầu
  * 
- * $i = 0; 0 nhỏ hơn 10 => TRUE; $result = 0 + 1 => $result = 1;
+ * Khởi tạo $i = 0; 0 nhỏ hơn 10 => TRUE; $result = 0 + 1 => $result = 1;
  * $i = 1; 1 nhỏ hơn 10 => TRUE; $result = 1 + 1 => $result = 2;
  * $i = 2; 2 nhỏ hơn 10 => TRUE; $result = 2 + 2 => $result = 4;
  * $i = 3; 3 nhỏ hơn 10 => TRUE; $result = 3 + 3 => $result = 6;
@@ -49,7 +49,7 @@ echo $result;
  */
 echo '<hr>';
 
-// Vòng lặp for giảm
+// Ví dụ: Vòng lặp for giảm
 for ($i = 20; $i > 0; $i--) {
     echo 'Đây là vòng lặp thứ ' . $i . '<br>';
 }
