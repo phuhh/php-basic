2d72de154299ab16c47ec0445e3fbb3d4b88e574
Phu - Company <phuhh2019@gmail.com>
2024-12-25 09:04:47 +0700

    fix small

--- module-05/manager-user/config.php
+++ module-05/manager-user/config.php
@@ -7,10 +7,10 @@ defined('_ACTION_DEFAULT') or define('_ACTION_DEFAULT', 'lists');
 defined('_ACCESS_DENIED') or define('_ACCESS_DENIED', true);
 
 // Thiết lập đường dẫn Host Root
-// defined('_WEB_HOST_ROOT') or define(
-//     '_WEB_HOST_ROOT',
-//     'http://' . $_SERVER['HTTP_HOST'] . '/php-basic/module-05/manager-user'
-// );
+defined('_WEB_HOST_ROOT') or define(
+    '_WEB_HOST_ROOT',
+    'http://' . $_SERVER['HTTP_HOST'] . '/php-basic/module-05/manager-user'
+);
 
 // defined('_WEB_HOST_ROOT') or define(
 //     '_WEB_HOST_ROOT',
